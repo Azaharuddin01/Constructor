a33ba29e16b2e974ce953304282fd4c8c23ab020
user <user@DESKTOP-3G4HHUT>
2022-02-21 03:39:30 -0800

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Constructor
+{
+    class MyApp
+    {
+        public string name;
+        public int var;
+        public MyApp(string name)
+        {
+            this.name = name;
+
+        }
+        public void get(int var)
+        {
+            this.var = var;
+            Console.WriteLine(this.name);
+            Console.WriteLine(this.var);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyApp obj = new MyApp("Azahar Uddin");
+            obj.get(5);
+
+        }
+    }
+}
